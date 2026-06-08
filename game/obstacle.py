436149b312112ec7c56c7820742f090cedf1b431
Ayyoub <77174228+Ayyoub-ch@users.noreply.github.com>
2026-06-08 20:00:45 +0200

    Modification de la position de l'obstacle

--- game/obstacle.py
+++ game/obstacle.py
@@ -2,9 +2,11 @@ import pygame
 
 class Obstacle(pygame.sprite.Sprite):
     
-    def __init__(self, image_path, speed):
+    def __init__(self, speed, ground_y_obstacle):
         super().__init__()
         self.speed = speed 
         self.image = pygame.image.load('assets/sprites/obstacle/barriere.png')
         self.rect = self.image.get_rect()
-        self.rect.x = 100
+        self.rect.x = 1000
+        self.rect.bottom = ground_y_obstacle
+        
